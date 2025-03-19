9631069f95a6c155e46735dafabb57a846ac79c5
𝕄𝔸𝕂𝕆ℝ𝔸 <c.lenotilus@gmail.com>
2025-03-19 15:18:04 +0000

    Add files via upload

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+        });
+        console.log('MongoDB connecté avec succès');
+    } catch (error) {
+        console.error('Erreur de connexion à MongoDB:', error.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB; 
